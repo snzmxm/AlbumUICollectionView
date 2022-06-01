32b8e9f6d598485f1a29cd183c1412e4799dd163
snzmxm <mxmdev96@gmail.com>
2022-06-01 15:29:40 +0700

    Initial Commit

---
+++ AlbumUICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumUICollectionView
+//
+//  Created by SNZ on 01.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
